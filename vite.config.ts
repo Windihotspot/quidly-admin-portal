623cbb327330dd072d90a298a8681d5060a82dbb
boiwindihotspot <boiwindihotspot@gmail.com>
2025-10-30 13:22:06 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/quidly-admin-portal',
+  base: '/',
   server: {
     port: 3001, 
   },
   plugins: [
     vue(),
   ],
-   optimizeDeps: {
-    include: [
-      '@fullcalendar/core',
-      '@fullcalendar/daygrid',
-      '@fullcalendar/timegrid',
-      '@fullcalendar/interaction',
-    ]
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+   
+
 })
